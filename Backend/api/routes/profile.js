a83c7c457598a48294ef480198a6abade662f6db
ayushi1115 <ayushi.singh@sjsu.edu>
2020-06-23 17:20:18 -0700

    added config files for docker

--- Backend/api/routes/profile.js
+++ Backend/api/routes/profile.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const Profile = require('../models/profile');
 const Role = require('../models/role');
-var multer = require('multer');
 const path = require("path");
 
 router.get('/', (req, res, next) => {
@@ -72,48 +71,10 @@ router.patch("/", (req, res, next) => {
 
 
 
-const storage = multer.diskStorage({
-    destination: "../frontend/public/uploads",
-    filename: function (req, file, cb) {
-        cb(null, "CANVAS" + Date.now() + path.extname(file.originalname));
-    }
-});
-
-const upload = multer({
-    storage: storage,
-    limits: { fileSize: 999999999999999999999999},
-}).single("myImage");
 
 
-router.post('/imgupload', (req, res, next) => {
-    upload(req, res, (err) => {
-
-        console.log("Request ---", req.body);
-        console.log("Request file ---", JSON.stringify(req.file));  //Here you get file.
-        var filepath = req.file;
-        var filepath = filepath.filename;
-        var email = req.body.email;
-
-        Profile.update({email : email}, { $set: {image : filepath}})
-        .exec()
-        .then(result => {
-            console.log(result);
-            res.status(200).json({
-                message:filepath
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                error:err
-            });
-        });
-       
 
 
-    });
-   
-});
 
 
 module.exports = router;
--- Backend/app.js
+++ Backend/app.js
@@ -9,11 +9,10 @@ const userRoutes = require('./api/routes/user');
 const profileRoutes = require('./api/routes/profile');
 const courseRoutes = require('./api/routes/course');
 const enrollRoutes = require('./api/routes/enroll');
-const assignmentRoutes = require('./api/routes/assignment');
-const quizRoutes = require('./api/routes/quiz');
-const announcementRoutes = require('./api/routes/announcement');
+
+
 const lectureRoutes = require('./api/routes/lecture');
-const submissionRoutes = require('./api/routes/submission');
+
 const permissionRoutes = require('./api/routes/permission');
 const mailRoutes = require('./api/routes/mail');
 var passport = require("passport");
@@ -34,7 +33,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json());
 //use cors to allow cross origin resource sharing
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors())
 
 
 app.use(passport.initialize());
@@ -54,11 +53,7 @@ app.use('/user', userRoutes);
 app.use('/profile',  profileRoutes);
 app.use('/course',  courseRoutes);
 app.use('/enroll',  enrollRoutes);
-app.use('/assignment',  assignmentRoutes);
-app.use('/quiz',  quizRoutes);
-app.use('/announcement',  announcementRoutes);
 app.use('/lecture',  lectureRoutes);
-app.use('/submission',  submissionRoutes);
 app.use('/permission',  permissionRoutes);
 app.use('/mail',  mailRoutes);
 
