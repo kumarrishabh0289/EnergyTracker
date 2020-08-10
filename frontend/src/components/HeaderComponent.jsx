3d7159ab4e08d5aace25674be1b52f167c274bf8
Chaitanya Naik <chaitanyauday.naik@sjsu.edu>
2020-08-10 16:50:55 -0700

    fix bg images not appearing on refresh

--- frontend/src/components/HeaderComponent.jsx
+++ frontend/src/components/HeaderComponent.jsx
@@ -8,24 +8,24 @@ class HeaderComponent extends Component {
         const isUserLoggedIn = AuthenticationForApiService.isUserLoggedIn();
         var teacher = false
         var student = false
-        if(isUserLoggedIn){
-            if (sessionStorage.role == "teacher"){
+        if (isUserLoggedIn) {
+            if (sessionStorage.role == "teacher") {
                 teacher = true
             }
-            if (sessionStorage.role == "student"){
+            if (sessionStorage.role == "student") {
                 student = true
             }
         }
-        
+
 
         return (
             <header>
                 <nav className="navbar navbar-expand-md bg-light navbar-light ">
-                    <div><img src="logo.png" height="50" width="120" alt="logo"></img> <a href="/" className="navbar-brand" >Energy Tracker</a></div>
+                    <div><img src="/logo.png" height="50" width="120" alt="logo"></img> <a href="/" className="navbar-brand" >Energy Tracker</a></div>
                     <ul className="navbar-nav">
-                        {student &&  <li><Link className="nav-link" to="/studentdashboard">Student Dashboard</Link></li>}
-                        {teacher &&  <li><Link className="nav-link" to="/teacherdashboard">Teacher Dashboard</Link></li>}
-                        
+                        {student && <li><Link className="nav-link" to="/studentdashboard">Student Dashboard</Link></li>}
+                        {teacher && <li><Link className="nav-link" to="/teacherdashboard">Teacher Dashboard</Link></li>}
+
                     </ul>
                     <ul className="navbar-nav navbar-collapse justify-content-end">
                         {!isUserLoggedIn && <li><Link className="nav-link" to="">Login</Link></li>}
