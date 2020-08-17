d9c8e965f5592156f2affb4626e468e418b02642
Kumar Rishabh <kumarrishabh@sjsu.edu>
2020-08-17 03:01:24 -0700

    changed url

--- frontend/src/Constants.js
+++ frontend/src/Constants.js
@@ -1,3 +1,3 @@
-//export const API_URL = 'http://Energytracker-env-1.eba-zc98baeb.us-east-2.elasticbeanstalk.com/api'
-export const API_URL = 'http://localhost:3000/api'
+export const API_URL = 'http://Energytracker-env-1.eba-zc98baeb.us-east-2.elasticbeanstalk.com/api'
+//export const API_URL = 'http://localhost:3000/api'
 
